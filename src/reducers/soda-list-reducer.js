7ee47286ae51414bec6120e1407193b43348071b
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-04 09:02:36 -0800

    test soda-list-reducer passed

---
+++ src/reducers/soda-list-reducer.js
@@ -0,0 +1,24 @@
+import * as c from './../actions/ActionTypes';
+
+export default (state = {}, action) => {
+  const { name, brand, sugarContent, pints, price, id } = action;
+  switch (action.type) {
+    case c.ADD_SODA:
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          sugarContent: sugarContent,
+          pints: pints,
+          price: price,
+          id: id
+        }
+      });
+    case c.DELETE_SODA:
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
+};
