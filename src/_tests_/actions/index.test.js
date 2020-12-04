5afb9b90799a6b624537ba68661e57eefbbf722a
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-04 10:58:16 -0800

    twelth test had a good fail

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -8,4 +8,10 @@ describe('soda fountain actions', () => {
       id: 1
     });
   });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 });
