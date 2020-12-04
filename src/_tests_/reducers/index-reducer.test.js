7521a69c4eba3b9f19cbae1cdd3c1a8af80f462d
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-04 10:32:47 -0800

    eighth test has passed from combining our reducers

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -17,4 +17,8 @@ describe("rootReducer", () => {
   test('Check that initial state of sodaListReducer matches root reducer', () => {
     expect(store.getState().masterSodaList).toEqual(sodaListReducer(undefined, { type: null }));
   });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 });
