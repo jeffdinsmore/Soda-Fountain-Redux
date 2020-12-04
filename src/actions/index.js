56e40b1b5443e3bae02faea48d5d0cdf1bc11806
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-04 11:03:09 -0800

    thirteenth test has passed from for action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,16 @@ export const deleteSoda = id => ({
 export const toggleForm = () => ({
   type: c.TOGGLE_FORM
 });
+
+export const addSoda = (soda) => {
+  const { name, brand, sugarContent, pints, price, id } = soda;
+  return {
+    type: c.ADD_SODA,
+      name: name,
+      brand: brand,
+      sugarContent: sugarContent,
+      pints: pints,
+      price: price,
+      id: id
+  }
+}
