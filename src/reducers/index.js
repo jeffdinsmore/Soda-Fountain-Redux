266ef5dacfaa43cdddd66c5673c7bef5f2a067c0
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-04 15:38:29 -0800

    done for the day, will return later to finish project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import sodaListReducer from './soda-list-reducer';
 import { combineReducers } from 'redux';
+import editingReducer from './editing-reducer';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterSodaList: sodaListReducer
+  masterSodaList: sodaListReducer,
+  editing: editingReducer
 });
 
 export default rootReducer;
