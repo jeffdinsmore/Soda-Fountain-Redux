ae90f7fdbfa2ea4a60a8fe69f38d6b6ab80a81c4
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-10 16:11:38 -0800

    updated reusable form to have default values when updating items

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -2,50 +2,92 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ReusableForm(props) {
-  
+  let formHandle;
+  if (props.soda) {
+    formHandle = <form onSubmit={props.formSubmissionHandler}>
+      <input className="field"
+        type='text'
+        name='name'
+        placeholder='Soda Name'
+        defaultValue={props.soda.name}
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='text'
+        name='brand'
+        placeholder='Brand'
+        defaultValue={props.soda.brand}
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='text'
+        name='sugarContent'
+        placeholder='List sugar content'
+        defaultValue={props.soda.sugarContent}
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='number'
+        name='pints'
+        placeholder='Total pints'
+        defaultValue={props.soda.pints}
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='text'
+        name='price'
+        placeholder='Price per pint'
+        defaultValue={props.soda.pints}
+        required='required' />
+      <br></br>
+      <button className="btn-success" type='submit'>{props.buttonText}</button>
+    </form>
+  } else {
+    formHandle = <form onSubmit={props.formSubmissionHandler}>
+      <input className="field"
+        type='text'
+        name='name'
+        placeholder='Soda Name'
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='text'
+        name='brand'
+        placeholder='Brand'
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='text'
+        name='sugarContent'
+        placeholder='List sugar content'
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='number'
+        name='pints'
+        placeholder='Total pints'
+        required='required' />
+      <br></br>
+      <input className="field"
+        type='text'
+        name='price'
+        placeholder='Price per pint'
+        required='required' />
+      <br></br>
+      <button className="btn-success" type='submit'>{props.buttonText}</button>
+    </form>
+  }
   return (
     <React.Fragment>
-      
-      <form onSubmit={props.formSubmissionHandler}>
-        <input className="field"
-          type='text'
-          name='name'
-          placeholder='Soda Name'
-          required='required' />
-          <br></br>
-        <input className="field"
-          type='text'
-          name='brand'
-          placeholder='Brand' 
-          required='required' />
-          <br></br>
-        <input className="field"
-          type='text'
-          name='sugarContent'
-          placeholder='List sugar content'
-          required='required' />
-          <br></br>
-        <input className="field"
-          type='number'
-          name='pints'
-          placeholder='Total pints' 
-          required='required' />
-          <br></br>
-        <input className="field"
-          type='text'
-          name='price'
-          placeholder='Price per pint'
-          required='required' />
-          <br></br>
-        <button className="btn-success" type='submit'>{props.buttonText}</button>
-      </form>
+      {formHandle}
     </React.Fragment>
   );
 }
 
 ReusableForm.propTypes = {
   formSubmissionHandler: PropTypes.func,
-  buttonText: PropTypes.string
+  buttonText: PropTypes.string,
+  soda: PropTypes.object
 };
 
 export default ReusableForm;
